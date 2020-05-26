b7c3c0d5455a5fc1281fa773c172cc5e50ad510e
w51fan <abc2978723@163.com>
2020-05-26 22:20:12 +0800

    0526

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ import purchaseConfirm from '@/pages/purchase/purchaseConfirm'
 Vue.use(Router)
 
 export default new Router({
+    mode: 'history',
     routes: [{
             path: '/home',
             name: 'home',
